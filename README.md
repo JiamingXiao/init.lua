2e020129b74f7d82272bbb898c2d135fe524ff02
Jiaming Xiao <jiaming_xiao@outlook.com>
2023-10-19 22:01:06 +0800

    Add pkg manager and colorthem

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # init.lua
 NeoVim configuration
-blah blah
--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,15 @@
 vim.opt.relativenumber = true
 vim.opt.number = true
+vim.opt.autoindent = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+
+-- package manager
+local Plug = vim.fn["plug#"]
+vim.call("plug#begin")
+Plug "navarasu/onedark.nvim"
+vim.call("plug#end")
+-- plugin setting
+require("onedark").load()
